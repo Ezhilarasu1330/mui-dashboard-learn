1edd8758face4848e6668a85e8dec9cd872368c8
Ezhilarasu <ezhil9841@gmail.com>
2020-12-22 10:03:22 +0530

    Initial Commit

---
+++ src/App/App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { makeStyles, CssBaseline, createMuiTheme, ThemeProvider } from '@material-ui/core';
+
+import './App.css';
+
+import SideMenu from "../Components/SideMenu";
+import Header from "../Components/Header";
+import Employees from '../Pages/Employees/Employees';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#333996",
+      light: '#3c44b126'
+    },
+    secondary: {
+      main: "#f83245",
+      light: '#f8324526'
+    },
+    background: {
+      default: "#f4f5fd"
+    },
+  },
+  overrides: {
+    MuiAppBar: {
+      root: {
+        transform: 'translateZ(0)'
+      }
+    }
+  },
+  props: {
+    MuiIconButton: {
+      disableRipple: true
+    }
+  }
+})
+
+
+const useStyles = makeStyles({
+  appMain: {
+    paddingLeft: '250px',
+    width: '100%'
+  }
+})
+
+function App() {
+  const classes = useStyles();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <SideMenu />
+      <div className={classes.appMain}>
+        <Header />
+        
+        <Employees />
+      </div>
+      <CssBaseline />
+    </ThemeProvider>
+  );
+}
+
+export default App;
